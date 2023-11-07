f464ff53a2f4030cd9720839cf2fef71d47dc0af
Patrick Nguyen <76218662+Pattypooper@users.noreply.github.com>
2023-11-06 16:01:26 -0800

    Updated App.js to make it more functional

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { Credentials } from './Credentials';
-import NavigationBar from './Components/NavigationBar';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Body from './Components/Body';
-import axios from 'axios';
+import NavigationBar from './Components/NavigationBar';
+import SignUpForm from './Components/SignUpForm';
+import Leaderboard from './Components/NavigationBar/Leaderboard';
 
-function App(props) {
-  const spotify = Credentials();
-  const [token, setToken] = useState('');
-
-  return (
-    <div className="App">
-      <NavigationBar />
-      <Body />
+const Home = () => {
+  return(
+    <div>
+      <NavigationBar/>
+      <Body/>
     </div>
+  )
+}
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="/account" element={<SignUpForm />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
