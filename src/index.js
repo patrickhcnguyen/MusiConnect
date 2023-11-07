96612ddf3e920d63e18b47f079e33d6ad1a4e91b
Patrick Nguyen <76218662+Pattypooper@users.noreply.github.com>
2023-11-06 23:52:07 -0800

    Added Firebase wrapping

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // Import ReactDOM
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Body from './Components/Body';
-import NavigationBar from './Components/NavigationBar';
-import SignUpForm from './Components/SignUpForm';
-import Leaderboard from './Components/NavigationBar/Leaderboard'
+import ReactDOM from 'react-dom';
+import App from './App';
+import { FirebaseContext } from './Firebase/FirebaseContext';
+import {firebaseApp} from './Firebase/config';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <NavigationBar />
-        <Routes>
-          <Route path="/" element={<Body />} />
-          <Route path="/home" element={<Body />} />
-          <Route path="/account" element={<SignUpForm />} />
-          {/* <Route path="/review" element={<Review />} /> */}
-           <Route path="/leaderboard" element={<Leaderboard />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-ReactDOM.render(<App/>, document.getElementById('root')); 
-
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <FirebaseContext.Provider value={{ firebase: firebaseApp }}>
+    <App />
+    </FirebaseContext.Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
